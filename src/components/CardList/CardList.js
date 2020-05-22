60c651075cea72b9fecb5399dbe121b2258b8a05
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-05-22 07:26:13 -0400

    error has been fixed

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -3,9 +3,6 @@ import Card from "../Card/Card";
 
 
 const CardList= ({cats}) => {
-    if (true) {
-        throw new Error("NOOO")
-    }
     const cardComponent = cats.map((cat,i) => {
         return  <Card key={cats[i].id} id={cats[i].id} name={cats[i].name} email={cats[i].email} />
     })
